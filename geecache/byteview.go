7436b0956b58e924e71fa08e6fe219ad6722c9ff
pojiang20 <1021465760@qq.com>
2022-08-24 22:30:53 +0800

    day3完

---
+++ geecache/byteview.go
@@ -0,0 +1,24 @@
+package geecache
+
+type ByteView struct {
+	b []byte
+}
+
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+//b是只读的，只能通过此方法返回副本，防止外部程序修改
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
