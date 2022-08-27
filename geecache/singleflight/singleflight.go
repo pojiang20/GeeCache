5e5ee6eadac9efc3661e47923e6800d7f5c25d32
pojiang20 <1021465760@qq.com>
2022-08-27 16:49:50 +0800

    day7完

--- geecache/singleflight/singleflight.go
+++ geecache/singleflight/singleflight.go
@@ -13,7 +13,8 @@ type Group struct {
 	m  map[string]*call
 }
 
-//保证fn()只会执行一次
+//当有一个key请求发起后,fn处理函数处理过程中,如果有其他相同key的请求过来
+//那么就一直阻塞等待第一个fn处理完成,拿到完成后的值。
 func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
 	g.mu.Lock()
 	if g.m == nil {
